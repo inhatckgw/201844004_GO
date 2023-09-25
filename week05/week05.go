f14091ee7f55efe4e7887d0d7f5d15af9c294510
inhatckgw <kgw990619@gmail.com>
2023-09-25 11:34:58 +0900

    week05_5

--- week05/week05.go
+++ week05/week05.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -21,6 +22,12 @@ func main() {
 
 	fmt.Print("Input Score: ")
 	reader := bufio.NewReader((os.Stdin))
-	input, _ := reader.ReadString('\n')
-	fmt.Println(input)
+	//input := reader.ReadString('\n') -> 복수의 변수를 갖는 ReadString
+	//input, err := reader.ReadString('\n') -> 변수가 사용안됨
+	//input, _ := reader.ReadString('\n') -> 사용할 변수가 한개인 경우 _(모든 에러 값 무시)으로 변수 선언 시 가능
+	inputScore, err := reader.ReadString('\n')
+	if err != nil { //conditional
+		log.Fatal(err) //에러 보고 후 프로그램 종료
+	}
+	fmt.Println(inputScore)
 }
