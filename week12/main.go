74506d352ce987c5d3fec321538a579d8fc1153f
inhatckgw <kgw990619@gmail.com>
2023-11-13 11:49:02 +0900

    V0.2

--- week12/main.go
+++ week12/main.go
@@ -6,21 +6,44 @@ func main() {
 	//var s []int
 	//s = make([]int, 5)
 
-	s := [5]int{1, 2, 3, 4, 5}
+	a := [5]int{1, 2, 3, 4, 5}
+	as := a[0:2]
+	as[1] = 8
+	fmt.Println(a)
 
-	s[0] = 99
-	s[4] = 11
+	b := []int{4, 3, 2, 1}
+	bs := b[0:]
+	bs[3] = 15
+	fmt.Println(b)
 
-	for i := 0; i < len(s); i++ {
-		fmt.Println(s[i])
-	}
+	c := make([]string, 4, 5) //length(4) != capacity(5)
+	c[0] = "a"
+	c[2] = "b"
+	c[3] = "c"
+	cs := c[0:2]
+	cs[1] = "z"
+	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &c[0], &cs[0], &c[1])
 
-	for _, value1 := range s {
-		fmt.Println(value1)
-	}
-
-	//slice1 := s[0:4]
-	// for _, value2 := range slice1 {
-	// 	fmt.Println(value2)
+	// for i := 0; i < len(s); i++ {
+	// 	fmt.Println(s[i])
 	// }
+
+	// for _, value1 := range s {
+	// 	fmt.Print(value1, " ")
+	// }
+	// fmt.Println(" ")
+
+	// slice1 := s[0:4]
+	// for _, value2 := range slice1 {
+	//  	fmt.Println(value2)
+	// }
+
+	// test := [3]string{"a", "b", "c"}
+	// testS := test[0:2]
+	// fmt.Println(len(testS))
+
+	// testS2 := test[0:]
+	// fmt.Println(testS2[1])
+
 }
